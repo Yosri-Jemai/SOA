8838a4e7d59d9afdbe4606b11a19fa13452967b5
unknown <yosrii.jemaiii@gmail.com>
2023-11-05 13:54:45 +0100

    'SOA_Test'

---
+++ YosriJemai_LSI3_SOA/src/main/java/service/CalculatriceWS.java
@@ -0,0 +1,12 @@
+package service;
+
+import jakarta.jws.*;
+
+@WebService
+public class CalculatriceWS {
+
+    @WebMethod(operationName = "somme")
+    public double somme(@WebParam(name = "a") double a, @WebParam(name = "b") double b) {
+        return a + b;
+    }
+}
